8dbc800afc2c7be5c768ea1aa71d362b406979af
LiuUexe <esternocliomastoidal@gmail.com>
2026-02-23 16:02:58 +0100

    Project created and first commit

---
+++ src/main/java/com/example/DentalPlus_Backend/DentalPlusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.DentalPlus_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalPlusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalPlusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DentalPlus_Backend/DentalPlusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DentalPlus_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalPlusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
